0985830683c2b80ebadd1c7f9666757edf15f0d1
aroy23 <arnavroy@ucla.edu>
2024-04-06 10:32:56 -0700

    Initial Commit

---
+++ BruinsHaveClass/BruinsHaveClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  BruinsHaveClassApp.swift
+//  BruinsHaveClass
+//
+//  Created by Arnav Roy on 4/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BruinsHaveClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
